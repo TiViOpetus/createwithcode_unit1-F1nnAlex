9bdcb8009bf0df232b7adef41b07b54d74d182fa
FinnAlex <alexodavies05@gmail.com>
2023-02-01 12:43:42 +0200

    fine tuned the first exercise

--- Unit 1/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Unit 1/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -10,17 +10,17 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    public float speed = 20.0f;
-    public float turnSpeed = 45.0f;
-    public float horizontalInput;
-    public float forwardInput;
-
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
