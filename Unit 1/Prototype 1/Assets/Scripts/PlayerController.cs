4dc49c5513921d5822d8217be511163278a163ec
FinnAlex <alexodavies05@gmail.com>
2023-02-01 12:30:44 +0200

    finished the exercise

--- Unit 1/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Unit 1/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -10,11 +10,17 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    public float speed = 20;
+    public float speed = 20.0f;
+    public float turnSpeed = 45.0f;
+    public float horizontalInput;
+    public float forwardInput;
 
     void Update()
     {
-        //Move the vehicle forward
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
